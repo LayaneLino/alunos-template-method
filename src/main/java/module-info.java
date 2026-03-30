aebc709c0af13cfcf13f815185343d8e3b7db885
LayaneLino <layanelinno@gmail.com>
2026-03-30 14:43:26 -0300

    Tirando package desnecessária

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.alunostm {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens org.alunostm.view to javafx.fxml;
+    opens org.alunostm to javafx.fxml;
     opens org.alunostm.controller to javafx.fxml;
-    exports org.alunostm.view;
+    exports org.alunostm;
 }
--- src/main/java/org/alunostm/HelloApplication.java
+++ src/main/java/org/alunostm/HelloApplication.java
@@ -1,4 +1,4 @@
-package org.alunostm.view;
+package org.alunostm;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
